1bdb5047edd1561fcc284dde28d41bc5dc4efc1e
Adam Gazdiev <a.gazdiev@gmail.com>
2023-12-06 22:59:30 +0100

    Added countdown

--- script.js
+++ script.js
@@ -76,3 +76,23 @@ document.addEventListener('DOMContentLoaded', function() {
     }, (index + 1) * 3000); // начальная задержка для каждого текста, чтобы анимации не совпадали
   });
 });
+
+//*Countdown*//
+
+function updateCountdown() {
+  const now = new Date();
+  const newYear = new Date('January 1, 2024 00:00:00');
+  const diff = newYear - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.getElementById('days').textContent = days.toString().padStart(2, '0');
+  document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
+  document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
+  document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+}
+
+setInterval(updateCountdown, 1000);
